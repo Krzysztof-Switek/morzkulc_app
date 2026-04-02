d238dfd9b91affe320cb6a9cb4c23a1dd18d42c6
Your Name <you@example.com>
2026-04-02 16:21:55 +0200

    wyświetlanie sprzętu na desctopie tak jak na mobile

--- public/sw.js
+++ public/sw.js
@@ -13,7 +13,7 @@
  * Stary cache jest automatycznie czyszczony w activate.
  */
 
-const CACHE_VERSION = "v18";
+const CACHE_VERSION = "v19";
 const STATIC_CACHE  = `morzkulc-static-${CACHE_VERSION}`;
 
 // Pliki precachowane przy instalacji SW
