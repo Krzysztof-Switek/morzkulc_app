27dc5c696db6e576a081db6c038f55cc93e9af82
Your Name <you@example.com>
2026-09-01 11:01:43 +0200

    wyświtlanie kalendaża admin basen

--- public/sw.js
+++ public/sw.js
@@ -14,7 +14,7 @@
  * Stary cache jest automatycznie czyszczony w activate.
  */
 
-const CACHE_VERSION = "mthiz1h9";
+const CACHE_VERSION = "mti955g1";
 const STATIC_CACHE  = `morzkulc-static-${CACHE_VERSION}`;
 
 // Pliki precachowane przy instalacji SW
